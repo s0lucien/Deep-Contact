5254e30a7aa0c8697305a9a010e9f554851faf1c
JianWu <jwu@csis.dk>
2018-02-25 19:29:17 +0100

    build xml for state

--- src/xml_writing/build_xml.py
+++ src/xml_writing/build_xml.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from xml.etree.ElementTree import Element, SubElement
 from xml.etree import ElementTree
 from xml.dom import minidom
 
 
-def body_2_xml(body):
+def _body_2_xml(body):
     body_xml = Element('body')
     body_xml.set('index', str(body.userData))
 
@@ -43,15 +45,53 @@ def body_2_xml(body):
 
     # shape
     # FIXME: Do we need shape? Since there are only moving circle
+    shape = SubElement(body_xml, 'shape')
+    if m > 0:
+        shape.set('value', 'circle')
+    else:
+        shape.set('value', 'ground')
 
     return body_xml
 
 
-def bodies_2_xml(bodies):
-    bodies_xml = Element('bodies')
-    bodies_xml.extend([
-        body_2_xml(body)
+def _contact_2_xml(contact, index):
+    contact_xml = Element('contact')
+    contact_xml.set('index', str(index))
+    contact.set("master", str(contact.fixtureA.body.userData))
+    contact.set("slave", str(contact.fixtureB.body.userData))
+
+    # position
+    pos = SubElement(contact_xml, "position")
+    pos.set('x', str(contact.manifold.localPoint.x))
+    pos.set("y", str(contact.manifold.localPoint.y))
+
+    # velocity
+    velocity = SubElement(contact_xml, 'velocity')
+    velocity.set('nx', str(contact.manifold.localPoint.x))
+    velocity.set('ny', str(contact.manifold.localPoint.y))
+
+    # force
+    force = SubElement(contact_xml, 'force')
+    force.set('n', str(n))
+    force.set('t', str(t))
+
+    # depth
+    d = SubElement(contact_xml, 'depth')
+    d.set('value', str(depth))
+
+    return contact_xml
+
+
+def config_xml(bodies, contacts):
+    config_xml = Element('Configuration')
+    config_xml.extend([
+        _body_2_xml(body)
         for body in bodies
     ])
 
-    return bodies_xml
+    config_xml.extend([
+        _contact_2_xml(contact)
+        for contact in contacts
+    ])
+
+    return config_xml
