fc347358ad15f69851882cf82850b7f7941c7c2e
lu ti <lucian.tirca@highstage.dk>
2018-03-06 04:54:01 +0100

    added more files to gitignore, added upload script in gen_data

---
+++ src/gen_data/upload.sh
@@ -0,0 +1,12 @@
+FILES=*.xml
+for f in $FILES
+	do
+	echo "Uploading $f file ..."
+	davix-put --userlogin $DCUNAME --userpass $DCPASS $f davs://io.erda.dk/DeepContact/$f
+	ret=$?
+	echo "return code $ret"
+	if [ $ret -ne 0 ]; then
+		echo "retrying ..."
+		davix-put --userlogin $DCUNAME --userpass $DCPASS $f davs://io.erda.dk/DeepContact/$f
+	fi
+done
