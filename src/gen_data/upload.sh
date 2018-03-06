f77bf7433ea81e2a4dd48c2db7709b8a8ac5173f
lu ti <lucian.tirca@highstage.dk>
2018-03-06 04:58:09 +0100

    upload to another folder

--- src/gen_data/upload.sh
+++ src/gen_data/upload.sh
@@ -2,11 +2,11 @@ FILES=*.xml
 for f in $FILES
 	do
 	echo "Uploading $f file ..."
-	davix-put --userlogin $DCUNAME --userpass $DCPASS $f davs://io.erda.dk/DeepContact/$f
+	davix-put --userlogin $DCUNAME --userpass $DCPASS $f davs://io.erda.dk/DeepContact/2DSim/$f
 	ret=$?
 	echo "return code $ret"
 	if [ $ret -ne 0 ]; then
 		echo "retrying ..."
-		davix-put --userlogin $DCUNAME --userpass $DCPASS $f davs://io.erda.dk/DeepContact/$f
+		davix-put --userlogin $DCUNAME --userpass $DCPASS $f davs://io.erda.dk/DeepContact/2DSim/$f
 	fi
 done
