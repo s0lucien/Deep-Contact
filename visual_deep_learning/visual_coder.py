6649cc31ba2211381552fcb12d75d75c9d30b47d
Jian Wu <xcb479@alumni.ku.dk>
2018-05-04 23:13:15 +0200

    model for encoder and decoder, still some error , fix later

--- visual_deep_learning/visual_coder.py
+++ visual_deep_learning/visual_coder.py
@@ -1,28 +1,130 @@
 # This file includes the model for visual encoder 
 # and visual decoder
 
-class visual_encoder(object):
-    def __init__(self, input):
-        self.input = input
+from keras.layers.noise import GaussianNoise
+from keras.layers.convolutional import (
+    Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D,
+)
+from keras.layers.normalization import BatchNormalization
+import keras.models as models
+from keras.layers.core import (
+    Layer, Dense, Dropout, Activation, Flatten, Reshape,
+)
+from keras.utils import np_utils
+from keras import backend as K
+import tensorflow as T
 
-    @staticmethod
-    def encoder_model():
-        # should be cnn here
-        pass
+import os
 
-    def state_encode(self):
-        # return state encode
-        pass
+os.environ['KERAS_BACKEND'] = 'theano'
+os.environ['THEANO_FLAGS'] = \
+	'mode=FAST_RUN, floatX=float32, optimizer=fast_compile'
 
 
-class visual_decoder:
-    def __init__(self, input):
-        self.input = input
+class UnPooling2D(Layer):
+    """
+	A 2D Repeat layer
+	Example:
+		| x y |   -->  | x x y y|
+		| z a |        | z z a a|
+	"""
+    def __init__(self, poolsize=(2, 2)):
+        super(UpSampling2D, self).__init__()
+        self.input = T.tensor4()
+        self.poolsize = poolsize
 
-    @staticmethod
-    def decoder_model():
-        # should be CNN here and return model
-        pass
+    @property
+    def output_shape(self):
+        input_shape = self.input_shape
+        return (input_shape[0], input_shape[1],
+                self.poolsize[0] * input_shape[2],
+                self.poolsize[1] * input_shape[3])
 
-    def state_decode(self):
-        pass
+    def get_output(self, train):
+        X = self.get_input(train)
+        s1 = self.poolsize[0]
+        s2 = self.poolsize[1]
+        output = X.repeat(s1, axis=2).repeat(s2, axis=3)
+        return output
+
+    def get_config(self):
+        return {"name":self.__class__.__name__,
+            "poolsize":self.poolsize}
+
+
+class AutoEncoder(object):
+	def __init__(self):
+		self.model = models.Sequential()
+
+	def build_model(self, input_shape):
+		self.model.add(Layer(input_shape=input_shape))
+		self.model.encoding_layers = self.create_encoding_layers()
+		self.model.decoding_layers = self.create_decoding_layers()
+
+		for layer in self.model.encoding_layers:
+			self.model.add(layer)
+
+		for layer in self.model.decoding_layers:
+			self.model.add(layer)
+
+	@staticmethod
+	def create_encoding_layers():
+		kernel = 3
+		pad = 1
+		pool_size = 2
+
+		return [
+			ZeroPadding2D(padding=(pad,pad)),
+			Convolution2D(64, kernel, kernel, border_mode='valid'),
+			BatchNormalization(),
+			Activation('relu'),
+			MaxPooling2D(pool_size=(pool_size, pool_size)),
+
+			ZeroPadding2D(padding=(pad,pad)),
+			Convolution2D(128, kernel, kernel, border_mode='valid'),
+			BatchNormalization(),
+			Activation('relu'),
+			MaxPooling2D(pool_size=(pool_size, pool_size)),
+
+			ZeroPadding2D(padding=(pad,pad)),
+			Convolution2D(256, kernel, kernel, border_mode='valid'),
+			BatchNormalization(),
+			Activation('relu'),
+			MaxPooling2D(pool_size=(pool_size, pool_size)),
+
+			ZeroPadding2D(padding=(pad,pad)),
+			Convolution2D(512, kernel, kernel, border_mode='valid'),
+			BatchNormalization(),
+			Activation('relu')
+		]
+
+	@staticmethod
+	def create_decoding_layers():
+		kernel = 3
+		pad = 1
+		pool_size = 2
+
+		return [
+			ZeroPadding2D(padding=(pad,pad)),
+			Convolution2D(512, kernel, kernel, border_mode='same'),
+			BatchNormalization(),
+
+			UpSampling2D(size=(pool_size,pool_size)),
+			ZeroPadding2D(padding=(pad,pad)),
+			Convolution2D(256, kernel, kernel, border_mode='same'),
+			BatchNormalization(),
+
+			UpSampling2D(size=(pool_size,pool_size)),
+			ZeroPadding2D(padding=(pad,pad)),
+			Convolution2D(128, kernel, kernel, border_mode='same'),
+			BatchNormalization(),
+
+			UpSampling2D(size=(pool_size,pool_size)),
+			ZeroPadding2D(padding=(pad,pad)),
+			Convolution2D(64, kernel, kernel, border_mode='same'),
+			BatchNormalization(),
+		]
+
+#a = AutoEncoder()
+#a.build_model(input_shape=(1, 50, 50))
+#print(a.model.summary)
