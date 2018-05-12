65f8266c6c90c1f1e4a11e5ea83345e9f652b664
Jian Wu <xcb479@alumni.ku.dk>
2018-05-12 17:02:52 +0200

    training

--- training.py
+++ training.py
@@ -9,102 +9,6 @@ from visual_deep_learning.visual_encoder import visual_encoder, state_decoder
 from visual_deep_learning.learning_model import dynamic_predictor
 
 
-def sess_train(FLAGS):
-    # Architecture Definition
-    # Frame
-    F = tf.placeholder(tf.float32,
-                       [None, 6, FLAGS.hight, FLAGS.wight, FLAGS.col_dim],
-                       name="F")
-    F1, F2, F3, F4, F5, F6 = tf.unstack(F, 6, 1)
-
-    # Future Data
-    label = tf.placeholder(tf.float32,
-                           [None, 8, FLAGS.No, 4],
-                           name="label")
-    label_part = tf.unstack(label, 8, 1)
-
-    # State Code
-    S_label = tf.placeholder(tf.float32,
-                             [None, 4, FLAGS.No, 4],
-                             name="S_label")
-    S_label_part = tf.unstack(S_label, 4, 1)
-
-    # discount factor
-    df = tf.placeholder(tf.float32, [], name="DiscountFactor")
-
-    # x and y coordinate channels
-    x_cor = tf.placeholder(tf.float32,
-                           [None, FLAGS.hight, FLAGS.wight, 1],
-                           name="x_cor")
-    y_cor = tf.placeholder(tf.float32,
-                           [None, FLAGS.hight, FLAGS.wight, 1],
-                           name="y_cor")
-
-    # Visual Encoder
-    S1, S2, S3, S4 = visual_encoder(
-        F1, F2, F3, F4, F5, F6, x_cor, y_cor, FLAGS)
-
-    # Rolling Dynamic Predictor
-    roll_num = FLAGS.roll_num
-    roll_in = tf.identity(tf.Variable(tf.zeros([roll_num]),
-                                      dtype=tf.float32))
-    roll_out = tf.scan(rollout_DP,
-                       roll_in,
-                       initializer=tf.stack([S1, S2, S3, S4], 0))
-    S_pred = tf.unstack(roll_out, 4, 1)[-1]
-    S_pred = tf.reshape(tf.stack(tf.unstack(S_pred, FLAGS.batch_num, 1),
-                                 0),
-                        [-1, FLAGS.No, FLAGS.Ds])
-
-    # State Decoder
-    S = tf.concat([S1, S2, S3, S4, S_pred], 0)
-    out_sd = state_decoder(S, FLAGS)
-    S_est = np.zeros(4, dtype=object)
-    for i in range(4):
-        S_est[i] = tf.slice(out_sd,
-                            [FLAGS.batch_num*i, 0, 0],
-                            [FLAGS.batch_num, -1, -1])
-    label_pred = tf.reshape(tf.slice(out_sd,
-                                     [FLAGS.batch_num*4, 0, 0],
-                                     [-1, -1, -1]),
-                            [FLAGS.batch_num, roll_num, FLAGS.No, 4])
-    label_pred8 = tf.unstack(label_pred, roll_num, 1)[:8]
-
-    # loss and optimizer
-    mse = df * tf.reduce_mean(
-        tf.reduce_mean(
-            tf.square(label_pred8[0] - label_part[0]), [1, 2])
-    )
-    for i in range(1, 8):
-        mse += (df ** (i+1)) * tf.reduce_mean(
-            tf.reduce_mean(tf.square(label_pred8[i] - label_part[i]), [1, 2]))
-    mse = mse/8
-
-    ve_loss = tf.reduce_mean(
-        tf.reduce_mean(
-            tf.square(S_est[0] - S_label_part[0]), [1, 2]))
-    for i in range(1, 4):
-        ve_loss += tf.reduce_mean(
-            tf.reduce_mean(
-                tf.square(S_est[i]-S_label_part[i]), [1, 2])
-        )
-    ve_loss = ve_loss/4
-
-    total_loss = mse + ve_loss
-    optimizer = tf.train.AdamOptimizer(0.0005)
-    trainer = optimizer.minimize(total_loss)
-
-    # tensorboard
-    tf.summary.scalar('tr_loss', total_loss)
-    merged = tf.summary.merge_all()
-    writer = tf.summary.FileWriter(FLAGS.log_dir)
-
-    sess = tf.InteractiveSession()
-    tf.global_variables_initializer().run()
-
-    return sess, trainer, merged, writer
-
-
 def rollout_DP(prev_out):
     # rolling DP to roll_num
     S1, S2, S3, S4 = tf.unstack(prev_out, 4, 0)
@@ -114,12 +18,211 @@ def rollout_DP(prev_out):
     return res
 
 
-def read_train_images(path, FLAGS, tr_ratio=1, val_ratio=0):
+class Trainer(object):
+    def __init__(self, FLAGS, tr_data, tr_label, tr_S_label):
+        self.config = FLAGS
+        self.optimizer = tf.train.AdamOptimizer(0.0005)
+        self.tr_data = tr_data
+        self.tr_label = tr_label
+        self.S_label = tr_S_label
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+
+    def train(self):
+        # Architecture Definition
+        # Frame
+        F = tf.placeholder(
+            tf.float32,
+            [None, 6, self.config.hight, FLAGS.wight, FLAGS.col_dim],
+            name="F")
+        F1, F2, F3, F4, F5, F6 = tf.unstack(F, 6, 1)
+
+        # Future Data
+        label = tf.placeholder(
+            tf.float32,
+            [None, 8, self.config.No, 4],
+            name="label")
+        label_part = tf.unstack(label, 8, 1)
+
+        # State Code
+        S_label = tf.placeholder(
+            tf.float32,
+            [None, 4, self.config.No, 4],
+            name="S_label")
+        S_label_part = tf.unstack(S_label, 4, 1)
+
+        # discount factor
+        df = tf.placeholder(tf.float32, [], name="DiscountFactor")
+
+        # x and y coordinate channels
+        x_cor = tf.placeholder(
+            tf.float32,
+            [None, self.config.hight, FLAGS.wight, 1],
+            name="x_cor")
+        y_cor = tf.placeholder(
+            tf.float32,
+            [None, self.config.hight, FLAGS.wight, 1],
+            name="y_cor")
+
+        # Visual Encoder
+        S1, S2, S3, S4 = visual_encoder(
+            F1, F2, F3, F4, F5, F6, x_cor, y_cor, self.config)
+
+        # Rolling Dynamic Predictor
+        roll_num = self.config.roll_num
+        roll_in = tf.identity(tf.Variable(tf.zeros([roll_num]),
+                                          dtype=tf.float32))
+        roll_out = tf.scan(rollout_DP,
+                           roll_in,
+                           initializer=tf.stack([S1, S2, S3, S4], 0))
+        S_pred = tf.unstack(roll_out, 4, 1)[-1]
+        S_pred = tf.reshape(
+            tf.stack(tf.unstack(S_pred, self.config.batch_num, 1), 0),
+            [-1, self.config.No, FLAGS.Ds])
+
+        # State Decoder
+        S = tf.concat([S1, S2, S3, S4, S_pred], 0)
+        out_sd = state_decoder(S, self.config)
+        S_est = np.zeros(4, dtype=object)
+        for i in range(4):
+            S_est[i] = tf.slice(out_sd,
+                                [self.config.batch_num*i, 0, 0],
+                                [self.config.batch_num, -1, -1])
+        label_pred = tf.reshape(tf.slice(out_sd,
+                                         [self.config.batch_num*4, 0, 0],
+                                         [-1, -1, -1]),
+                                [self.config.batch_num, roll_num, FLAGS.No, 4])
+        label_pred8 = tf.unstack(label_pred, roll_num, 1)[:8]
+
+        # loss and optimizer
+        mse = df * tf.reduce_mean(
+            tf.reduce_mean(
+                tf.square(label_pred8[0] - label_part[0]), [1, 2])
+        )
+        for i in range(1, 8):
+            mse += (df ** (i+1)) * tf.reduce_mean(
+                tf.reduce_mean(
+                    tf.square(label_pred8[i] - label_part[i]),
+                    [1, 2],
+                )
+            )
+        mse = mse/8
+
+        ve_loss = tf.reduce_mean(
+            tf.reduce_mean(
+                tf.square(S_est[0] - S_label_part[0]), [1, 2]))
+        for i in range(1, 4):
+            ve_loss += tf.reduce_mean(
+                tf.reduce_mean(
+                    tf.square(S_est[i]-S_label_part[i]), [1, 2])
+            )
+        ve_loss = ve_loss/4
+
+        total_loss = mse + ve_loss
+        optimizer = self.optimizer()
+        trainer = optimizer.minimize(total_loss)
+
+        # tensorboard
+        tf.summary.scalar('tr_loss', total_loss)
+        merged = tf.summary.merge_all()
+        writer = tf.summary.FileWriter(self.config.log_dir)
+
+        sess = tf.InteractiveSession()
+        tf.global_variables_initializer().run()
+
+        xcor, ycor = self.xy_cor()
+
+        for i in range(self.config.max_epoches):
+            df_value = 1
+            tr_loss = 0
+            tr_loss2 = 0
+
+            for j in range(len(self.tr_data)/self.config.batch_num):
+                batch_data = self.tr_data[
+                    j*self.config.batch_num:(j+1)*FLAGS.batch_num]
+                batch_label = self.tr_label[
+                    j*self.config.batch_num:(j+1)*FLAGS.batch_num]
+                batch_S_label = self.tr_S_label[
+                    j*self.config.batch_num:(j+1)*FLAGS.batch_num]
+
+                if(j == 0):
+                    estimated, summary, tr_loss_part, tr_loss_part2, _ = sess.run(
+                        [label, merged, mse, ve_loss, trainer],
+                        feed_dict={
+                            F: batch_data,
+                            label: batch_label,
+                            S_label: batch_S_label,
+                            x_cor: xcor,
+                            y_cor: ycor,
+                            df: df_value,
+                        },
+                    )
+                    writer.add_summary(summary, i)
+                else:
+                    tr_loss_part, tr_loss_part2, _ = sess.run(
+                        [mse, ve_loss, trainer],
+                        feed_dict={
+                            F: batch_data,
+                            label: batch_label,
+                            S_label: batch_S_label,
+                            x_cor: xcor,
+                            y_cor: ycor,
+                            df: df_value,
+                        },
+                    )
+                tr_loss += tr_loss_part
+                tr_loss2 += tr_loss_part2
+
+                log_msg = """
+                    Epoch: {}, Traing mse: {}, Training ve loss: {}
+                """
+                print(
+                    log_msg.format(
+                        str(i+1),
+                        str(tr_loss/(int(len(self.tr_data)/self.config.batch_num))),
+                        str(tr_loss2/int(len(self.tr_data)/self.config.batch_num)),
+                    )
+                )
+
+    def test(self):
+        pass
+
+    def xy_cor(self):
+        # x-cor and y-cor setting
+        nx, ny = (self.config.wight, self.config.hight)
+
+        x = np.linspace(0, 1, nx)
+        y = np.linspace(0, 1, ny)
+
+        xv, yv = np.meshgrid(x, y)
+        xv = np.reshape(xv, [self.config.hight, self.config.wight, 1])
+        yv = np.reshape(yv, [self.config.hight, self.config.wight, 1])
+
+        xcor = np.zeros(
+            (self.config.batch_num*5, self.config.hight, self.config.wight, 1),
+            dtype=float,
+        )
+        ycor = np.zeros(
+            (self.config.batch_num*5, self.config.hight, self.config.wight, 1),
+            dtype=float,
+        )
+        for i in range(self.config.batch_num*5):
+            xcor[i] = xv
+            ycor[i] = yv
+
+        return xcor, ycor
+
+
+def load_data(path, FLAGS, tr_ratio=1, val_ratio=0):
     '''
     parameters:
         path: traing image path
-        tr_ratio = percentage of training images
-        val_ratio = percentage of validation images
+        tr_ratio: percentage of training images
+        val_ratio: percentage of validation images
 
     return:
         tr_data, tr_label, tr_S_label
@@ -255,6 +358,10 @@ if __name__ == '__main__':
         help='Maximum limitation of epoches',
     )
     parser.add_option(
+        '--No', type=int, default=100, dest='No',
+        help='The number of moving objects',
+    )
+    parser.add_option(
         '--Ds', type=int, default=64, dest='Ds',
         help='The State Code Dimension',
     )
@@ -268,7 +375,6 @@ if __name__ == '__main__':
     parser.add_option(
         '--hight', type=int, help='hight',
     )
-
     FLAGS, args = parser.parse_args()
     FLAGS.No = 2
     print(xy_cor(FLAGS))
